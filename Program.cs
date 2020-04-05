45f1e5b84dbf383e620456ad00c8dee829dd35d5
tolmasbek <62470053+tolmasbek@users.noreply.github.com>
2020-04-05 22:15:46 +0500

    first commit, created class Converter

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace dz_2020_04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  • Задание 2
+             *      A. Создайте проект по шаблону Console Application.
+             *      B. Cоздать класс Converter.
+             *          1. В теле класса создать пользовательский конструктор, который принимает три
+             *          вещественных аргумента, и инициализирует поля соответствующие курсу 3-х
+             *          основных валют, по отношению к сомони – public Converter(double
+             *          usd(доллар), double eur(евро), double rub(рубль)).
+             *
+             *          Написать программу, которая будет выполнять конвертацию из сомони в
+             *          одну из указанных валют, также программа должна производить
+             *          конвертацию из указанных валют в сомони.
+             */
+           
+            Console.ReadKey();
+        }
+    }
+    class Converter
+    {
+        private double Unusd { get; set; }
+        private double Eueur { get; set; }
+        private double Rurub { get; set; }
+        public Converter(double usd, double eur, double rub)
+        {
+               
+        }
+    }
+}
